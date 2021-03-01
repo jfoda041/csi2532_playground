e3b2d493224f4d503d916663adc00090b84bfed0
Jacob Fodale (étudiant) <71846266+jfoda041@users.noreply.github.com>
2021-03-01 17:11:41 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -13,10 +13,10 @@ CREATE TABLE schema_migrations (
 );
 
 INSERT INTO schema_migrations (migration, migrated_at) VALUES
-('20200127173000-create-athletes.sql', '2020-01-27 17:30:00');
+('20210301143000-create-athletes.sql', '2021-03-01 14:30:00');
 
 INSERT INTO schema_migrations (migration, migrated_at) VALUES 
-('20200126174000-create-migrations.sql', '2020-01-27 17:40:00');
+('20210301143500-create-migrations.sql', '2021-03-01 14:35:00');
 
 ALTER TABLE athletes
 RENAME COLUMN identified_gender TO gender;
@@ -32,3 +32,6 @@ CREATE TABLE Competitions (
     duration time(0),
     PRIMARY KEY(compid)
 );
+
+INSERT INTO schema_migrations (migration, migrated_at) VALUES
+('20210301144500-create-migrations.sql', '2021-03-01 14:45:00');
